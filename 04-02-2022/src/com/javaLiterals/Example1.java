df65262568022c0d3054679af43fb6c1011f9260
Pravalika <Pravalika@DESKTOP-CJIU4FR>
2022-02-03 15:00:47 +0530

    javaliterals we have write in a program on 04-02-2022

---
+++ 04-02-2022/src/com/javaLiterals/Example1.java
@@ -0,0 +1,45 @@
+package com.javaLiterals;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		//Literals
+		//Write a java program to learner(Id,name.Adress,age) using java literal
+		
+		int learnerId = 2989;
+		String  learnerName = "Pravalika";
+		int learnerAge = 24;
+		String learnerAddress = "Husnabad";
+		float learnerMarks = 7.09f;
+		boolean lernerResult =  true;
+		char learnerGrade = 'A';
+		
+		
+		System.out.print("learnerId");
+		System.out.println("learnerId=2989");
+		
+		System.out.print("learnerName");
+		System.out.println("learnerName=Pravalika");
+
+		System.out.print("learnerAge");
+		System.out.println("learnerAge=24");
+
+		System.out.print("learnerAddress");
+		System.out.println("learnerAddress=Husnabad");
+
+		System.out.print("learnerMarks");
+		System.out.println("learnerMarks=7.09");
+
+		System.out.print("learnerResult");
+		System.out.println("learnerResult=true");
+
+		System.out.print("learnerGrade");
+		System.out.println("learnerGrade=A");
+
+		
+		
+
+	}
+
+}
